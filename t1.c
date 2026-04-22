4aa8a71033db58bd6cd3793d31b3dcd58bf22e47
Chepe-01 <perezjosealain@gmail.com>
2026-04-21 22:23:33 -0600

    Create t1.c

---
+++ t1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Dato{
+    int d;
+    struct Dato *ptrsing;
+};
+
+int menu(){
+    printf("1. Crear dato\n");
+    printf("2. Mostrar dato\n");
+    printf("3. Liberar\n");
+    printf("4. Salir\n");
+    printf("seleccione una opcion");
+    scanf("%d",&op);
+    return op;
+}
+struct Dato * reservarmemoria(){
+    struct Dato *ptrtemp;
+    ptrtemp=(struct Dato*)malloc(sizeof(struct Dato));
+    if(ptrtemp==NULL){
+        return NULL;
+    }elsE{
+        
+    }
+}
+int main (void){
+    int op;
+    struct Dato *ptr=NULL;
+    
+    do{
+        op=menu();
+        switch (op){
+    case 1:
+        creardato()
+        }
+    }
+}
