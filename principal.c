3f358e031d17f826210793c337c091f405206070
Chepe-01 <perezjosealain@gmail.com>
2026-06-19 08:40:16 -0600

    Create principal.c

---
+++ principal.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "menu.h"
+struct Persona{
+    char *nombre;
+    int edad;
+    char genero;
+    char fn[8];
+    struct Alumno *ptrAlum;
+    struct Persona *ptrSig;
+};
+
+struct Alumno{
+    char matricula[10];
+    char carrera[5];
+    char semestre;
+    char correo[23];
+    float calif [5][5];
+};
+
+int main(){
+    int op;
+    struct Persona *ptr;
+
+    do{
+        op=munuPrincipal();                                           
+        switch (op){
+            case 1:
+                Altas(&ptr);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+
+    }while(op!=5);
+}
+
+//construir base de datos, 
+//dar de alta a una persona o puede ser un alumno. una persona no necesariamente es un alumno
+// 
