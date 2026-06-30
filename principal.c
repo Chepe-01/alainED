7e64bfd5ee778188c0fad2f34e4e6d3bc9ada98e
Chepe-01 <perezjosealain@gmail.com>
2026-06-29 23:36:32 -0600

    Update principal.c

--- principal.c
+++ principal.c
@@ -25,9 +25,12 @@ struct Alumno{
 #include "alta.h"
 #include "Bajas.h"
 #include "mostrar.h"
+#include "FuncionesEspeciales.h"
+#include "modificar.h"
+#include "Salir.h"
 
 int main(){
-    int op,op2,op3,op4;
+    int op,op2,op3,op4,op5,op6,op7;
     struct Persona *ptr=NULL;
 
     do{
@@ -96,12 +99,74 @@ int main(){
                         buscarMatricula(ptr);
                     break;
                     case 7:
-                        printf("Regresando al menu principal...\n");
+                        printf("Regresando...\n");
                     break;
                     default:
-                        printf("Opcion invalida, intente de nuevo.\n");
+                        printf("Opcion invalida.\n");
                     }
                 } while(op4 != 7);
+            break;
+           case 4:
+            do {
+            op5 = MenuFuncionesEspeciales();
+            switch (op5) {
+                case 1:
+                    do {
+                        op6 = MenuModificar();
+                        switch (op6) {
+                            case 1:
+                                do {
+                                    op7 = MenuModificarCalificaciones();
+                                    switch (op7) {
+                                        case 1:
+                                            modificarPorParcial(ptr);
+                                            break;
+                                        case 2:
+                                            modificarPorMateria(ptr);
+                                            break;
+                                        case 3:
+                                            printf("\nRegresando...\n");
+                                            break;
+                                    }
+                                } while(op7 != 3);
+                                break;
+
+                            case 2:
+                                do {
+                                    op7 = MenuModificarDatos();
+                                    switch (op7) {
+                                        case 1:
+                                            modificarDatosPersona(ptr);
+                                            break;
+                                        case 2:
+                                            modificarDatosAlumno(ptr);
+                                            break;
+                                        case 3:
+                                            printf("\nRegresando...\n");
+                                            break;
+                                    }
+                                } while(op7 != 3);
+                                break;
+
+                            case 3:
+                                printf("\nRegresando...\n");
+                                break;
+                        }
+                    } while(op6 != 3);
+                    break;
+
+                case 2:
+                    printf("\nApartado no disponibles.\n");
+                    break;
+
+                case 3:
+                    printf("\nRegresando...\n");
+                    break;
+                }
+            } while(op5 != 3);
+            break;
+            case 5:
+                Salir(&ptr);
             break;
             default:
                 printf("opcion invalida");
